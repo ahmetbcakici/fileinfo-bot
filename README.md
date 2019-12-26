fee2cf20d8cb42a4f5ae05e32e83903dfd8f6fa0
ahmetbcakici <ahmetbcakici@gmail.com>
2019-12-26 19:03:48 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">A discord bot which gives information about file extensions</h1>
+<p align="center">
+<img src="https://user-images.githubusercontent.com/36089310/71481157-c996a380-280d-11ea-9f18-cfe31edc3906.gif">
+</p>
---
+++ index.js
@@ -0,0 +1,44 @@
+const cheerio = require("cheerio");
+const request = require("request");
+const Discord = require("discord.js");
+const token = "NjU5NTAzOTI3NTQ0MjUwMzc5.XgTPUQ.iMxEa-WKkIXWU638mn5VqHJxGTg"; //bot key
+
+const client = new Discord.Client();
+
+client.on('message', (msg) => {
+    if (msg.content.substring(0, 9) === "!fileinfo") {
+        var message = msg.content;
+        var words = message.split(' ');
+
+        request(`https://fileinfo.com/extension/${words[1]}`, function(error, response, body) {
+            const $ = cheerio.load(body);
+            const siteBody = $('body');
+            msg.channel.send(siteBody.find('div.infoBox:nth-child(4) > p:nth-child(1) > span:nth-child(1)').text());
+        });
+    }
+
+    if (msg.content.substring(0, 6) === "!timer") {
+        var message = msg.content;
+        var words = message.split(' ');
+
+        var type = words[1].slice(-1);
+        var limit = words[1].slice(0, words[1].length - 1);
+
+        if (type == 'm') limit *= 60;
+        let counter = 0;
+
+        var tmr = setInterval(function() {
+            if (counter >= limit) {
+                clearInterval(tmr);
+                msg.channel.send("Time is up!");
+            }
+            counter++;
+        }, 1000);
+    }
+})
+
+client.on('ready', () => {
+    console.log("Bot is running!");
+})
+
+client.login(token);
